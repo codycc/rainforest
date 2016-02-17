f3c9f1f349745ab0c9c45dbd73d5c3f1ba92d223
Cody Condon <codycondon50@gmail.com>
2016-02-17 16:19:35 -0500

    implementajax and search function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
 
-
+root 'products#index'
    resources :products do
      resources :reviews, only: [:show, :create, :destroy]
    end
    resources :users, only: [:new, :create]
    resources :sessions, only: [:new, :create, :destroy]
-
 end
